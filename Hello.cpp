c33106859238888507978d4bc4e7ac76b35adf34
sarthak-gupta-sg <sarthakgupta@live.com>
2019-08-12 17:08:05 +0530

    Practice

--- Hello.cpp
+++ Hello.cpp
@@ -2,7 +2,23 @@
 
 int main()
 {
-	cout << "Hello Git" << std::endl << "Press any key to continue:" ;
+	int num = 0;
+	cout << "Enter an integer:"; 
+	cin >> num;
+	cout << "The square root of a number is:" << sqrt(num) << std::endl;
+
+	string name1, name2;
+	cout << "Enter two strings:"; 
+	cin >> name1 >> name2;
+
+	if ( name1 == name2)
+		cout << "Both strings are same" << endl;
+	else if(name1 > name2)
+		cout << name1 << " is lexicographically greater than " << name2 << endl;
+	else
+		cout << name1 << " is lexicographically smaller than " << name2 << endl;
+
+	cout << "Press any key to continue:" ;
 	keep_window_open();
 	return 0;
 }
