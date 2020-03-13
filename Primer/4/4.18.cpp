889d91b80338c5c852dbee18435e7fb3a45d02c4
sarthak-gupta-sg <sarthakgupta@live.com>
2020-03-13 14:26:45 +0530

    New Work

--- Primer/4/4.18.cpp
+++ Primer/4/4.18.cpp
@@ -8,6 +8,9 @@ int main()
     //Program to print all element of a vector til the first negative element
     vector<int> v { 5, 10, 23, 45, -6, -7};
     
+    auto beg = v.begin();
+    while(beg != v.end() && *beg >= 0)
+        cout << *++beg << " " << endl << flush;
 
     return 0;
 }
