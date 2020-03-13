41f4aba50c4f4283388ace80069a87d966bc3d2f
sarthak-gupta-sg <sarthakgupta@live.com>
2020-03-13 14:16:43 +0530

    New Work

---
+++ Primer/4/4.18.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    //Program to print all element of a vector til the first negative element
+    vector<int> v { 5, 10, 23, 45, -6, -7};
+    
+
+    return 0;
+}
