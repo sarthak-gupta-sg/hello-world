3371695852ea5d977cc69c90cb37d57ee51106ef
Sarthak Gupta <sarthakgupta@live.com>
2020-03-25 15:14:39 +0530

    Update

--- Primer/4/4.20.cpp
+++ Primer/4/4.20.cpp
@@ -10,7 +10,15 @@ int main()
     auto iter = v.begin();
 
     cout << *iter++ << endl;
-    cout << (*iter)++ << endl;
+    
+    //Illegal for strings as they do not have ++ defined
+    //cout << (*iter)++ << endl;
+    
+    //Illegal. . binds tighter than * thus iter.empty() is evaluated first. iter is a pointer (or iterator). It has no member named empty()
+    //cout << *iter.empty() << endl;
+    
+    cout << "True is: " << true << " and False is: " << false << endl; 
+    if(iter->empty
 
     return 0;
 }
