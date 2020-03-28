6e24da40d06b977269aff6843368d9999910f000
Sarthak Gupta <sarthakgupta@live.com>
2020-03-28 16:51:33 +0530

    New

---
+++ Primer/5/5.12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    while(cin >> ch)
+    {
+        if(ch == 'f')
+        {
+            cin >> ch;
+            switch(ch)
+            {
+                case 'f': case 'l' : case 'i' : 
+
+    return 0;
