1ad55285700d12ea7351147cf6ee805b3c69f106
Sarthak Gupta <sarthakgupta@live.com>
2020-04-03 17:59:30 +0530

    New

---
+++ Primer/6/6.17.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void stringLower(string & data)
+{
+    for
+
+}
+
+bool stringCapitalAny(string const & data)
+{
+    for(auto & x : data)
+    {
+        if(isupper(x))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    string data {""};
+    cout << "Enter any string: ";
+    cin >> data;
+    
+    cout << "Capital Letters: " << boolalpha << stringCapitalAny(data); 
+    
+    stringLower(data);
+    cout << " and string converted to lower: " << data << endl;
+    return 0;
+}
