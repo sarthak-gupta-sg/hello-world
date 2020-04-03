bd242a505fb4a9fb7b868a796866f00eb3f470d5
Sarthak Gupta <sarthakgupta@live.com>
2020-04-03 22:40:01 +0530

    New

--- Primer/6/6.22.cpp
+++ Primer/6/6.22.cpp
@@ -2,15 +2,31 @@
 
 using namespace std;
 
-void swap(int *m, int *n)
+//References to pointers
+void swap(int * & m, int * & n)
 {
     int *temp = nullptr;
     temp = m;
     m = n;
     n = temp;
+    
+    cout << "INSIDE REF TO PTR SWAP FUNCTION:-" << endl;
+    cout << "a: " << *m << " and b: " << *n << endl;
     return;
 }
 
+//Pointers to pointers
+void swap(int ** m, int ** n)
+{
+    int *temp = nullptr;
+    temp = *m;
+    *m = *n;
+    *n = temp;
+    
+    cout << "INSIDE PTR TO PTR SWAP FUNCTION:-" << endl;
+    cout << "a: " << **m << " and b: " << **n << endl;
+    return;
+}
 
 int main()
 {
@@ -19,9 +35,16 @@ int main()
 
     int *pa = &a,
         *pb = &b;
-        
+
+    cout << "ORIGINAL VALUES:-" << endl;
     cout << "a: " << *pa << " and b: " << *pb << endl;
+    
     swap(pa, pb);
+    cout << "AFTER REF TO PTR SWAP FUNCTION:-" << endl;
+    cout << "a: " << *pa << " and b: " << *pb << endl;
+    
+    swap(&pa, &pb);
+    cout << "AFTER PTR TO PTR SWAP FUNCTION:-" << endl;
     cout << "a: " << *pa << " and b: " << *pb << endl;
     return 0;
 }
