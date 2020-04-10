c806c4a936ba0e6c3061f1afd3011866b285b8b3
Sarthak Gupta <sarthakgupta@live.com>
2020-04-10 23:16:30 +0530

    New Work

--- Primer/7/7.1.cpp
+++ Primer/7/7.1.cpp
@@ -11,6 +11,33 @@ int main()
     if(cin >> sales.bookNo >> sales.unitsSold >> sales.revenue)
     {
         salesData trans;    //Variable to hold running sum
+        
+        //Read and process remaining transactions
+        while(cin >> trans.bookNo >> trans.unitsSold >> trans.revenue)
+        {
+            //If we are still processing the same book
+            if(sales.bookNo == trans.bookNo)
+            {
+                sales.unitsSold += trans.unitsSold;
+                sales.revenue += trans.revenue;
+            }
+            else
+            {
+                //Print results for previous book
+                cout << "ISBN: " << sales.bookNo << endl
+                     << "Units sold: " << sales.unitsSold << endl
+                     << "Total revenue: " << sales.revenue << endl
+                     << endl;
+                     
+                sales = trans;
+            }
+        }
+        
+        //Print the last transaction
+        cout << "ISBN: " << sales.bookNo << endl
+             << "Units sold: " << sales.unitsSold << endl
+             << "Total revenue: " << sales.revenue << endl
+             << endl;
     }
     else
     {
@@ -18,6 +45,10 @@ int main()
         cerr << "No data!" << endl;
         return -1;
     }
+    
+    const int a {1};
+    int * const p = &a;
+    cout << *p << endl;
 
     return 0;
 }
