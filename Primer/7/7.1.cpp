0bac159f0691a81c97948aac5ade9040bd35ec7f
Sarthak Gupta <sarthakgupta@live.com>
2020-04-10 18:45:27 +0530

    New Work

--- Primer/7/7.1.cpp
+++ Primer/7/7.1.cpp
@@ -5,8 +5,19 @@ using namespace std;
 
 int main()
 {
-    salesData sales;
-
+    salesData sales;    //Variable to hold data for next transaction
+    
+    //read the first transaction and ensure that there are data to process
+    if(cin >> sales.bookNo >> sales.unitsSold >> sales.revenue)
+    {
+        salesData trans;    //Variable to hold running sum
+    }
+    else
+    {
+        //No input! warn the user
+        cerr << "No data!" << endl;
+        return -1;
+    }
 
     return 0;
 }
