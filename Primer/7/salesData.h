20ac3a37d0fef37eafe34206e5087a905e6c9bc0
Sarthak Gupta <sarthakgupta@live.com>
2020-04-14 19:13:54 +0530

    New Work

--- Primer/7/salesData.h
+++ Primer/7/salesData.h
@@ -8,6 +8,18 @@ struct salesData
     std::string bookNo;
     unsigned int unitsSold = 0;
     double revenue = 0.0;
+    
+    std::string isbn() const
+    {
+        return bookNo;
+    }
+    
+    void combine(salesData object)
+    {
+        this->unitsSold += object.unitsSold;
+        this->revenue += object.revenue;
+        return;
+    }
 };
 
 #endif
