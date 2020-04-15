d734f865ac892d07a6705e163fb8ad0b1e71c8f2
Sarthak Gupta <sarthakgupta@live.com>
2020-04-15 18:29:04 +0530

    New Work

---
+++ Primer/7/Person.h
@@ -0,0 +1,22 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+class Person
+{
+    std::string name;
+    std::string address;
+    
+    std::string getName() const
+    {
+        return name;
+    }
+
+    std::string getAddress() const
+    {
+        return address;
+    }
+};
+
+#endif
