64d26a699f72ec8091b4965e6f49e0846329414b
Sarthak Gupta <sarthakgupta@live.com>
2020-04-16 22:50:16 +0530

    New Work

---
+++ Primer/7/salesData726.h
@@ -0,0 +1,80 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include <string>
+#include <iostream>
+
+//Bring the constructer with istream inside the class
+
+//We need to declare the function read before struct definition since read is used by constructor
+class salesData;
+std::istream& read(std::istream & in, salesData & sales);
+
+class salesData
+{
+    std::string bookNo;
+    unsigned int unitsSold = 0;
+    double revenue = 0.0;
+    
+    inline double avg_price() const
+    {
+        return unitsSold ? (revenue / unitsSold) : 0;
+    }
+    
+public:
+    //Default constructor
+    salesData() = default;
+    
+    //All paramters filled
+    salesData(std::string s, unsigned n, double r) : bookNo(s), unitsSold(n), revenue(r) {}
+    
+    //One paramter filled
+    salesData(std::string s) : bookNo(s) {}
+        
+    //Using istream
+    salesData(std::istream & in)
+    {
+        read(in, *this);
+    }
+    
+    std::string isbn() const
+    {
+        return bookNo;
+    }
+    
+    void combine(salesData object)
+    {
+        this->unitsSold += object.unitsSold;
+        this->revenue += object.revenue;
+        return;
+    }
+
+friend std::istream& read(std::istream & in, salesData & sales);
+friend std::ostream& print(std::ostream & out, salesData const & sales);
+};
+
+//Non Member friend functions
+std::istream& read(std::istream & in, salesData & sales)
+{
+    in >> sales.bookNo >> sales.unitsSold >> sales.revenue;
+    return in;
+}
+
+std::ostream& print(std::ostream & out, salesData const & sales)
+{
+    out << "ISBN: " << sales.isbn() << std::endl
+        << "Units sold: " << sales.unitsSold << std::endl
+        << "Total revenue: " << sales.revenue << std::endl; 
+    return out;
+}
+
+//Non Member functions
+//Not declared as friend since it doesn't access private members of salesData directly
+salesData add(salesData lhs, salesData rhs)
+{
+    salesData sum {lhs};
+    sum.combine(rhs);
+    return sum;
+}
+
+#endif
