313338e36650065a573fd60e79bb36dd496a5182
Sarthak Gupta <sarthakgupta@live.com>
2020-04-16 15:19:18 +0530

    New Work

---
+++ Primer/7/7.13.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "salesData711.h"
+
+using namespace std;
+
+//Call using a.out < input.txt > output.txt to read from input.txt and store output to output.txt
+int main()
+{
+    //Variable to hold data for next transaction
+    
+    //salesData sales;
+    salesData sales("Z");
+    //salesData sales("Z", 1, 1);
+    
+    //read the first transaction and ensure that there are data to process
+    if(cin >> sales.bookNo >> sales.unitsSold >> sales.revenue)
+    {
+        salesData trans;    //Variable to hold running sum
+        
+        //Read and process remaining transactions
+        while(read(cin, trans))
+        {
+            //If we are still processing the same book
+            if(sales.isbn() == trans.isbn())
+            {
+                sales = add(sales, trans);
+            }
+            else
+            {
+                //Print results for previous book
+                print(cout, sales) << endl;
+                sales = trans;
+            }
+        }
+        
+        //Print the last transaction
+        print(cout, sales) << endl;
+    }
+    else
+    {
+        //No input! warn the user
+        cerr << "No data!" << endl;
+        return -1;
+    }
+    
+    return 0;
+}
---
+++ Primer/7/Person715.h
@@ -0,0 +1,36 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+struct Person
+{
+    std::string name;
+    std::string address;
+    
+    std::string getName() const
+    {
+        return name;
+    }
+
+    std::string getAddress() const
+    {
+        return address;
+    }
+};
+
+//Non Member functions
+std::istream& read(std::istream & in, Person & object)
+{
+    in >> object.name >> object.address;
+    return in;
+}
+
+std::ostream& print(std::ostream & out, Person const & object)
+{
+    out << "Name: " << object.getName() << std::endl
+        << "Address: " << object.getAddress() << std::endl; 
+    return out;
+}
+
+#endif
