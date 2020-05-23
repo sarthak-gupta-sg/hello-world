cfaf20253378d1fcfbbc60ca044cfcc36749c9b5
Sarthak Gupta <sarthakgupta@live.com>
2020-05-23 23:26:35 +0530

    New Work

--- Primer/9/9.28.cpp
+++ Primer/9/9.28.cpp
@@ -3,8 +3,28 @@
 
 using namespace std;
 
-int main()
+void search(forward_list<string> & fl, string const & term)
 {
-    forwars_list<string> flist { " ", " ", " " }; 
+    cout << term << "\n";
+    auto curr = fl.begin();
+    auto prev = fl.before_begin();
+
+    while(curr != fl.end())
+    {
+        if(*curr == term)
+        {
+            cout << "HIT\n";
+        }
+        ++curr;
+        ++prev;
+    }
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    forward_list<string> flist { "One", "Two", "Three" }; 
+    
+    search(flist, string(argv[1]));
     return 0;
 }
