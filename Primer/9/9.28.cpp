68fd20e9875f2c68bd8c39a5f779d196868c1c23
Sarthak Gupta <sarthakgupta@live.com>
2020-05-24 11:51:49 +0530

    New Work

--- Primer/9/9.28.cpp
+++ Primer/9/9.28.cpp
@@ -3,28 +3,52 @@
 
 using namespace std;
 
-void search(forward_list<string> & fl, string const & term)
+void search(forward_list<string> & fl, string const & term1, string const & term2)
 {
-    cout << term << "\n";
+    bool found = false;
     auto curr = fl.begin();
     auto prev = fl.before_begin();
 
     while(curr != fl.end())
     {
-        if(*curr == term)
+        if(*curr == term1)
         {
             cout << "HIT\n";
+            prev = fl.insert_after(curr, term2);
+            curr++;
+            found = true;
         }
-        ++curr;
-        ++prev;
+        else
+        {
+            ++curr;
+            ++prev;
+        }
+    }
+    
+    if(! found)
+    {
+        curr = fl.insert_after(prev, term2);
     }
     return;
 }
 
 int main(int argc, char *argv[])
 {
-    forward_list<string> flist { "One", "Two", "Three" }; 
+    if(argc != 3)
+    {
+        cout << "Usage: <prog.out> term1 term2\n";
+        return 0;
+    }
     
-    search(flist, string(argv[1]));
+    forward_list<string> flist { "One", "Two", "Three", "Two" }; 
+    
+    search(flist, string(argv[1]), string(argv[2]));
+    
+    //Print the forward_list
+    for(auto const &x : flist)
+    {
+        cout << x << " " ;
+    }
+    cout << "\n";
     return 0;
 }
