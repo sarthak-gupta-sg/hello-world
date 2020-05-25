c653ef71a0b6a2300c85cd4f76790d4523ffae09
Sarthak Gupta <sarthakgupta@live.com>
2020-05-25 21:00:35 +0530

    New Work

---
+++ Primer/9/9.52.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    string expression { "1+(2*3)-5"};
+    stack<char> s;
+    
+    bool flag = false;
+    for(auto const & x : expression)
+    {
+        s.push(x);
+        if(x == '(')
+        {
+            flag = true;
+        }
+
+        if(flag && x == ')')
+        {
+            while(s.top() != '(')
+            {
+                //Pop till (
+                s.pop();
+            }
+            
+            //Pop the (
+            s.pop();
+            
+            s.push('#');
+        }
+    }
+    
+    string output;
+    for (; !s.empty(); s.pop()) 
+    {
+        output.insert(output.begin(), s.top());
+    }
+    cout << output << endl;
+    
+    return 0;
+}
