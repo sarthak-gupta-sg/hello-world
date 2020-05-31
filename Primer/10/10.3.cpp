498328459b2ab6a7a375ae990b55ccfd79183569
Sarthak Gupta <sarthakgupta@live.com>
2020-05-31 21:32:43 +0530

    Standard Library Algorithms

--- Primer/10/10.3.cpp
+++ Primer/10/10.3.cpp
@@ -6,20 +6,24 @@ using namespace std;
 
 int main()
 {
-    vector<int> values;
-    int result {0};
-    int terms {0};
+    vector<double> values;
+    double result {0};
+    double terms {0};
     
     cout << "Enter Integers (x to stop):\n";
     while(cin >> terms)
     {
         /* This statement can't be used 
-         * because 
+         * because ASCII value of character 'x' is 120
+         * Therefore, if user enters 120 as integer value, then
+         * the loop will break
          */  
         //if(terms == 'x') break;
         values.push_back(terms);
     }
     
+    cin.clear();
+    cin.ignore();
     result = accumulate(values.cbegin(), values.cend(), 0);
     cout << "Your result is: " << result << "\n";
     
