29316b487514e2e2b387e027a2773fe3b0673db3
Sarthak Gupta <sarthakgupta@live.com>
2020-06-01 21:09:19 +0530

    Standard Library Algorithms

--- Primer/10/10.5.cpp
+++ Primer/10/10.5.cpp
@@ -7,14 +7,28 @@ using namespace std;
 
 int main()
 {
-    vector<char * > roster1 { "abc", "def", "ghi"};
-    list<char * > roster2(roster1.cbegin(), roster1.cend());
-    int result;
+    vector<const char * > roster1 { "abc", "def", "ghi"};
+    list<const char * > roster2 { "abc", "def", "ghi"};
+    //list<const char * > roster2(roster1.cbegin(), roster1.cend());
+    bool result;
     
     result = equal(roster1.cbegin(), roster1.cend(), roster2.cbegin());
     cout << "Your result is: " << result << "\n";
     
+    std::cout << (void *) roster1[0] << std::endl;
+    std::cout << (void *) roster2.front() << std::endl;
+    
+    char const * c1 = "abcd";
+    char const * c2 = "abcd";
+    
+    if(c1 == c2)
+    {
+        cout << (void *)c1 << " and " << (void *)c2 << " are equal\n" ;
+    }
+    else
+    {
+        cout << (void *)c1 << " and " << (void *)c2 << " are not equal" ;
+    }
+     
     return 0;
 }
-    
-    
