417e2258e0dc8c0a3d6430c4a38bbedf97a18364
Sarthak Gupta <sarthakgupta@live.com>
2021-04-26 18:06:42 +0530

    New exercise

--- Primer/10/10.20.cpp
+++ Primer/10/10.20.cpp
@@ -9,7 +9,9 @@ int main()
 	std:: cout << "Enter required size of words to be searched in the vector: " << std::flush;
 	std::cin >> sz;
 	
+	auto lambda { [sz] ( std::string const & word ) -> bool { return word.size() >= sz; }  };
 	
-	
+	auto count { std::count_if( w.begin(), w.end(), lambda ) };
+	std::cout << "Number of words that fulfill criteria are: " << count << std::endl;
 	return 0;
 }
