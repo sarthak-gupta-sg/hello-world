889c79ae6acbeb62544d9ef364a325153056a636
Sarthak Gupta <sarthakgupta@live.com>
2021-04-26 11:09:08 +0530

    New exercise

---
+++ Primer/10/10.16.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{	
+	std::vector<std::string> w { "the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle" };
+	auto sz { 0 };
+	std:: cout << "Enter required size of word to be searched in the vector: " << std::flush;
+	std::cin >> sz;
+	
+	auto lambda { [sz]( std::string const & term ) -> bool { return term.size() >= sz ; } };
+	
+	auto iter { std::partition( w.begin(), w.end(), lambda ) };
+	
+	std::cout << "Number of elements that fulfill criteria are: " << iter - w.begin() << std::endl << std::flush;
+	std::cout << "Elements are as follows:" << std::endl << std::flush;
+	for( auto x = w.begin(); x != iter; ++x )
+	{
+		std::cout << *x << "\n" ;
+	}
+	std::cout << std::endl << std::endl << std::flush;
+	
+	return 0;
+}
