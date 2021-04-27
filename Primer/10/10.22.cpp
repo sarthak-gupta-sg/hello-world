08760be6967a1b54bfeefc5b1e9d484bb17211f6
Sarthak Gupta <sarthakgupta@live.com>
2021-04-27 12:02:47 +0530

    Update

--- Primer/10/10.22.cpp
+++ Primer/10/10.22.cpp
@@ -16,9 +16,15 @@ int main()
 	std:: cout << "Enter required size of words to be searched in the vector: " << std::flush;
 	std::cin >> sz;
 	
+	//std::count_if takes a unary predicate, so we use std::bind to create a partial application function bindFn
+	//bindFn takes 1 argument, that is the string.
+	//checkSize takes two arguments, string and the int
+	//std::placeholders::_1 means that 1st argument of bindFn, is passed on to checkSize as 1st argument of checkSize.
+	//2nd argument of checkSize is BOUND to value of sz
 	auto bindFn { std::bind( checkSize, std::placeholders::_1, sz ) };
 	
 	auto count { std::count_if( w.begin(), w.end(), bindFn ) };
+	
 	std::cout << "Number of words that fulfill criteria are: " << count << std::endl;
 	return 0;
 }
