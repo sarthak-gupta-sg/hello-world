cf286fbe2fc2acfaf5d4e1ecebfb7632b56b4cc6
Sarthak Gupta <sarthakgupta@live.com>
2021-04-28 08:54:11 +0530

    Update

--- Primer/10/10.25.cpp
+++ Primer/10/10.25.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <functional>
 
 bool checkSize( std::string const & word, int const & sz ) 
 {
 	return word.size() >= sz; 
 }  
-
 
 int main()
 {	
@@ -15,7 +15,18 @@ int main()
 	std:: cout << "Enter required size of words to be searched in the vector: " << std::flush;
 	std::cin >> sz;
 	
-	auto iter { std::partition( w.begin(), w.end(), 
+	auto bindFn { std::bind( checkSize, std::placeholders::_1, sz ) };
+	
+	auto iter { std::partition( w.begin(), w.end(), bindFn ) };
+	
+	std::cout << "Number of elements that fulfill criteria are: " << iter - w.begin() << std::endl << std::flush;
+	std::cout << "Elements are as follows:" << std::endl << std::flush;
+	for( auto x = w.begin(); x != iter; ++x )
+	{
+		std::cout << *x << "\n" ;
+	}
+	std::cout << std::endl << std::endl << std::flush;
+		
 	
 	return 0;
 }
