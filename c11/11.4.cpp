fe9b377754bad54a83539934bb7b3e141c74cbe9
Sarthak Gupta <sarthakgupta@live.com>
2021-05-01 19:25:49 +0530

    Update

--- c11/11.4.cpp
+++ c11/11.4.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
 #include <map>
+#include <algorithm>
+#include <cctype>
+
+//Old Style
+void methodOne( std::string & word )
+{
+	//Lowercase full word
+	for( auto & c : word )
+	{
+		c = std::tolower(c);
+	}
+		
+	//Check punctuation at end
+	if( *( word.end() - 1 ) == ',' || *( word.end() - 1 ) == '.' )
+	{
+		word.erase( word.end() - 1 );
+	}
+	
+	return;
+}
+
+//Using C++ erase remove idiom
+//https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
+void methodTwo( std::string & word )
+{
+	//Lowercase full word
+	for( auto & c : word )
+	{
+		c = std::tolower(c);
+	}
+
+	//std::ispunct only takes unsigned char (strictly) so as a template we need to ensure we are providing unsigned char to it
+	//See https://en.cppreference.com/w/cpp/string/byte/ispunct
+	auto iter = std::remove_if( word.begin(), word.end(), []( unsigned char c ) -> bool { return std::ispunct( c ); } );
+
+	//Other way is to use ispunct() from C library (i.e. without std::)
+	//auto iter = std::remove_if( word.begin(), word.end(), ispunct );	
+	
+	word.erase( iter, word.end() );
+	
+	return;
+}
 
 int main()
 {
@@ -9,21 +51,13 @@ int main()
 	//Press return followed by ctrl-d in Linux to invalidate input stream
 	while(std::cin >> word)
 	{
-		//Lowercase full word
-		std::replace( word.begin(), word.end(), []( char c ) -> char { return std::tolower( c ); } );
+		//methodOne( word );
 		
-		/*
-		for( auto c : word )
-		{
-			c = std::tolower(c);
-		}
-		*/ 
+		methodTwo( word );
 		
+		//Add to map
 		++wordCnt[word];
 	}
-	
-	
-	
 	
 	std::cout << "Word count is:" << std::endl;
 	//element is of type std::pair
