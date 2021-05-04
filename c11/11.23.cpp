bc6fc6623f8f7278348c1fd3c84cd2865f4a600a
Sarthak Gupta <sarthakgupta@live.com>
2021-05-04 15:00:42 +0530

    New exercise

---
+++ c11/11.23.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::multimap<std::string, std::vector<std::string>> famNameChildren;
+	
+	//Add families and children
+	famNameChildren.insert( { "Smith", { "One", "Two", "Three" } } );
+	famNameChildren.insert( { "Gosling", { "One" } } );
+	famNameChildren.insert( { "Smith", { "Four", "Five" } } );
+	
+	for( auto const & element : famNameChildren )
+	{
+		std::cout << element.first << std::flush << std::endl; 
+		for( auto const & children : element.second )
+		{
+			std::cout << "+" << children << std::flush << std::endl;
+		}
+	}
+	 
+	return 0;
+}
